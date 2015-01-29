219d8b962c010dae3035f0da7fc3973e448fba8b
Dylan <dylanpetiinfo@gmail.com>
2015-01-29 19:10:24 +1300

    added db credentials

--- core/wp-config.php
+++ core/wp-config.php
@@ -19,24 +19,22 @@ error_reporting(0);
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'localaboutus');
+define('DB_NAME', 'aboutus');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'DylanPeti');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'social7a0');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'dylanpeti.cjak8lq3y5cf.us-west-1.rds.amazonaws.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
-
-define('FS_METHOD', 'direct');
 
 
 
