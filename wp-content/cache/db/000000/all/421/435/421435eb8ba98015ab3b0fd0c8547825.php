85a0781969b49d173f04d2f02611b5df7b199e31
Dylan <dylanpetiinfo@gmail.com>
2015-01-29 15:21:17 +1300

    uploads added

---
+++ wp-content/cache/db/000000/all/421/435/421435eb8ba98015ab3b0fd0c8547825.php
@@ -0,0 +1,6 @@
+82ªS<?php exit; ?>a:6:{s:10:"last_error";s:0:"";s:10:"last_query";s:129:"
+					SELECT term_taxonomy_id
+					FROM wp_term_taxonomy
+					WHERE taxonomy = 'category'
+					AND term_id IN (22,474,754,6)
+				";s:11:"last_result";a:4:{i:0;O:8:"stdClass":1:{s:16:"term_taxonomy_id";s:1:"6";}i:1;O:8:"stdClass":1:{s:16:"term_taxonomy_id";s:2:"22";}i:2;O:8:"stdClass":1:{s:16:"term_taxonomy_id";s:3:"484";}i:3;O:8:"stdClass":1:{s:16:"term_taxonomy_id";s:3:"767";}}s:8:"col_info";a:1:{i:0;O:8:"stdClass":13:{s:4:"name";s:16:"term_taxonomy_id";s:7:"orgname";s:16:"term_taxonomy_id";s:5:"table";s:16:"wp_term_taxonomy";s:8:"orgtable";s:16:"wp_term_taxonomy";s:3:"def";s:0:"";s:2:"db";s:13:"AwsAboutusOne";s:7:"catalog";s:3:"def";s:10:"max_length";i:3;s:6:"length";i:20;s:9:"charsetnr";i:63;s:5:"flags";i:49699;s:4:"type";i:8;s:8:"decimals";i:0;}}s:8:"num_rows";i:4;s:10:"return_val";i:4;}
