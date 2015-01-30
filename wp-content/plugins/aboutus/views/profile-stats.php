3480eec0e86b5577b35cb5575853ab8fa2bc1716
Balazs Sebesteny <sebestenybalazs@gmail.com>
2015-01-30 22:21:25 +1300

    ABOUTUS-4 Local enviroment setup ABOUTUS-1 Wrong data displayed on the right side column

--- wp-content/plugins/aboutus/views/profile-stats.php
+++ wp-content/plugins/aboutus/views/profile-stats.php
@@ -1,37 +1,7 @@
-            <li class="visits">
-              <span class="icon"></span>
-              <span class="value">
-                  <?php 
-                  if(isset($stats[0]['value'])){
-                  echo intval($stats[0]['value']);
-                  } ?>
-              </span>
-              <span class="caption">Visits</span>
+            <?php foreach( $stats as $stat ) : ?>
+            <li class="<?php echo $stat['key']; ?>">
+                <span class="icon"></span>
+                <span class="value"><?php echo $stat['value']; ?></span>
+                <span class="caption"><?php echo $stat['title']; ?></span>
             </li>
-            <li class="emails">
-              <span class="icon"></span>
-              <span class="value">
-              <?php 
-              if(isset($stats[1]['value'])){
-              echo intval($stats[1]['value']); 
-              }
-              ?>
-              </span>
-              <span class="caption">Emails</span>
-            </li>
-            <li class="maps">
-              <span class="icon"></span>
-              <span class="value">
-              <?php 
-              if(isset($stats[2]['value'])){
-              echo intval($stats[2]['value']);
-              } 
-              ?>
-              </span>
-              <span class="caption">Map Views</span>
-            </li>
-            <li class="social">
-              <span class="icon"></span>
-              <span class="value"><?php if(isset($stats[3])) { echo intval((int)$stats[3]['value'] + (int)$stats[4]['value']); } ?></span>
-              <span class="caption">Social</span>
-            </li>
+            <?php endforeach; ?>
